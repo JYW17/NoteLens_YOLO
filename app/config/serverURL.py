ad127e2c35b74b764ac288bebcd266d069223283
Ju Yeongun <cutr1005@naver.com>
2025-02-18 13:21:24 +0900

    Delete URL.py

--- app/config/serverURL.py
+++
@@ -1,8 +0,0 @@
-# 서버 URL을 저장하는 파일
-
-SERVER2_HEALTH_URL = "http://43.203.54.176:8000/api/test/health"        # 서버2 정상작동 확인 주소
-SERVER2_OCR_MULTI_URL = "http://43.203.54.176:8000/api/ocr/ocr-multi"   # 이민섭 ocr서버 api 주소
-# SERVER2_OCR_MULTI_URL = "http://43.203.93.209:8000/api/ocr/ocr-multi"   # 주영운 ocr서버 api 주소 !!사용 안 함!!
-
-CLOVA_OCR_URL = "https://qdposcdt9j.apigw.ntruss.com/custom/v1/34862/1fcae2eeff11f93fcea05bc167c8784d74d9d7201f162f686bfb7c3df85d6bcf/general"
-
